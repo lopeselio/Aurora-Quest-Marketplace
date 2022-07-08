33b4e2e20d40defb98ecfb1d348003023765cb4b
lopeselio <lopeselio@gmail.com>
2022-07-08 18:26:35 +0530

    added config and token byte code

---
+++ config.js
@@ -0,0 +1,8 @@
+
+
+
+// export const nftaddress = "0xAfE41f4A53C09934e6541086E1e7BE78f6F4D87C"
+// export const nftmarketaddress ="0x8543d8F8e028db4e99EC72a7Ad2bE16D65948DC1"
+
+export const nftaddress = "0x5c089556FA8814E4A7065188C6bc33FFB6274cA1"
+export const nftmarketaddress ="0x19e741b2d04cd79E1e163cFE7027b02A2B8A6EE5"
